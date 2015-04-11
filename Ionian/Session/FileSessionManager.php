00bc5cb6bacbf73410d79bc9dcb63bde1d07dc01
demonshalo <rotm_2009@hotmail.com>
2015-04-11 14:25:44 -0700

    Switched SessionManager into a singleton!

--- Ionian/Session/FileSessionManager.php
+++ Ionian/Session/FileSessionManager.php
@@ -4,11 +4,34 @@ namespace Ionian\Session;
 
 class FileSessionManager implements SessionInterface {
 
-    public function start() {
-        if (!headers_sent() && session_start()) {
-            return true;
+    protected static $instance = null;
+
+    private function __construct(){}
+
+    private function __clone(){}
+
+    /**
+     * @return FileSessionManager|null Null is returned in case sessions are disabled according to PHP_SESSION_DISABLED constant
+     */
+    public static function getInstance(){
+        if (!isset(self::$instance)) {
+            $sm = new FileSessionManager;
+            if($sm->start())
+                self::$instance = $sm;
         }
-        return false;
+
+        return self::$instance;
+    }
+
+    private function start() {
+        if(session_status() == PHP_SESSION_DISABLED)
+            return false;
+
+        else if (session_status() == PHP_SESSION_NONE) {
+            return session_start();
+        }
+
+        return true;
     }
 
     public function getSession() {
--- Ionian/Session/SessionInterface.php
+++ Ionian/Session/SessionInterface.php
@@ -3,7 +3,6 @@
 namespace Ionian\Session;
 
 Interface SessionInterface{
-    public function start();
     public function addRecord($key, $value);
     public function removeRecord($key);
     public function destroy();
