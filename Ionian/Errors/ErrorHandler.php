c1c26fbfeb60565f91caa30ae3443e29bacaf2e9
demonshalo <rotm_2009@hotmail.com>
2015-09-27 12:18:30 +0200

    removed the old Ionian for a rewrite

--- Ionian/Errors/ErrorHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Ionian\Errors;
-
-abstract class ErrorHandler implements ErrorHandlerInterface{
-    protected $protocol;
-    function __construct(){
-        $this->protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1');
-    }
-}
--- Ionian/Session/SessionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ionian\Session;
-
-Interface SessionInterface{
-    public function addRecord($key, $value);
-    public function removeRecord($key);
-    public function destroy();
-}
