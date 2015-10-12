873eebbe3a329048af76cc87810b34fc48f87079
demonshalo <rotm_2009@hotmail.com>
2015-10-12 02:20:37 +0200

    added some new stuff

--- Ionian/Core/Controller.php
+++ Ionian/Core/Controller.php
@@ -1,7 +1,15 @@
 <?php
 namespace Ionian\Core;
 
+use PDO;
+
 abstract class Controller{
+    protected $db;
+
+    function __construct(PDO $db = null){
+        $this->db = $db;
+    }
+
     public static function outputJSON($response = "NO MSG PROVIDED!", $data = []){
         header('Content-Type: application/json');
         if(empty($data)) $data = false;
--- Ionian/Utils/IO.php
+++ Ionian/Utils/IO.php
@@ -2,7 +2,14 @@
 namespace Ionian\Utils;
 
 Class IO {
-    public static function getRemoteFileInfo($url, $returnObject = true){
+    /**
+     * Returns a remote URL information by getting the headers
+     *
+     * @param $url
+     * @param bool $returnObject Default = false. Set to true if you want an STD-class returned
+     * @return array|object
+     */
+    public static function getRemoteFileInfo($url, $returnObject = false){
         $headers = get_headers($url, true);
 
         if($returnObject)
